3a20c5d7a566421f3aa6294e15edb7154d794e3a
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2021-03-26 21:39:32 +0700

    ref: move white label to another file

--- api_app.go
+++ api_app.go
@@ -1,34 +1,5 @@
 package hellosign
 
-const (
-	// WhiteLabelingOptionsPageBackgroundColor is label for page background color
-	WhiteLabelingOptionsPageBackgroundColor = "page_background_color"
-	// WhiteLabelingOptionsHeaderBackgroundColor is label for header background color
-	WhiteLabelingOptionsHeaderBackgroundColor = "header_background_color"
-	// WhiteLabelingOptionsTextColor1 is label for options text color1
-	WhiteLabelingOptionsTextColor1 = "text_color1"
-	// WhiteLabelingOptionsTextColor2 is label for options text color2
-	WhiteLabelingOptionsTextColor2 = "text_color2"
-	// WhiteLabelingOptionsLinkColor is label for link color
-	WhiteLabelingOptionsLinkColor = "link_color"
-	// WhiteLabelingOptionsPrimaryButtonColor is label for primary button color
-	WhiteLabelingOptionsPrimaryButtonColor = "primary_button_color"
-	// WhiteLabelingOptionsPrimaryButtonTextColor is label for primary button text color
-	WhiteLabelingOptionsPrimaryButtonTextColor = "primary_button_text_color"
-	// WhiteLabelingOptionsPrimaryButtonColorHover is label for primary button color hover
-	WhiteLabelingOptionsPrimaryButtonColorHover = "primary_button_color_hover"
-	// WhiteLabelingOptionsPrimaryButtonTextColorHover is label for primary button text color hover
-	WhiteLabelingOptionsPrimaryButtonTextColorHover = "primary_button_text_color_hover"
-	// WhiteLabelingOptionsSecondaryButtonColor is label for secondary button color
-	WhiteLabelingOptionsSecondaryButtonColor = "secondary_button_color"
-	// WhiteLabelingOptionsSecondaryButtonTextColor is label for secondary button text color
-	WhiteLabelingOptionsSecondaryButtonTextColor = "secondary_button_text_color"
-	// WhiteLabelingOptionsSecondaryButtonColorHover is label for secondary button color hover
-	WhiteLabelingOptionsSecondaryButtonColorHover = "secondary_button_color_hover"
-	// WhiteLabelingOptionsSecondaryButtonTextColorHover is label for secondary button text color hover
-	WhiteLabelingOptionsSecondaryButtonTextColorHover = "secondary_button_text_color_hover"
-)
-
 // APIAppList represent list of api apps response
 type APIAppList struct {
 	APIApps  []APIApp `json:"api_apps"`
