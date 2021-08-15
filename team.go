0985f26c3b09e6889bf9fb97eb074b3ba987915d
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2021-08-15 16:31:13 +0700

    feat: add create and update team

--- team.go
+++ team.go
@@ -1,8 +1,10 @@
 package hellosign
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
+	"mime/multipart"
 	"net/http"
 )
 
@@ -18,7 +20,12 @@ type Team struct {
 
 const (
 	// subURLTeam is a sub url path for team
-	subURLTeam = "/team"
+	subURLTeam        = "/team"
+	subURLTeamCreate  = "/team/create"
+	subURLTeamDestroy = "/team/destroy"
+
+	// teamFieldName is a field for team name
+	teamFieldName = "name"
 )
 
 func (t *TeamAPI) Get(ctx context.Context) (Team, error) {
@@ -30,6 +37,70 @@ func (t *TeamAPI) Get(ctx context.Context) (Team, error) {
 		},
 	)
 
+	if err != nil {
+		return Team{}, err
+	}
+
+	team := Team{}
+	err = json.NewDecoder(resp.Body).Decode(&team)
+	if err != nil {
+		return Team{}, err
+	}
+
+	return team, nil
+}
+
+func (t *TeamAPI) Create(ctx context.Context, teamName string) (Team, error) {
+	var payload bytes.Buffer
+	writer := multipart.NewWriter(&payload)
+
+	teamNameField, err := writer.CreateFormField(teamFieldName)
+	if err != nil {
+		return Team{}, err
+	}
+	teamNameField.Write([]byte(teamName))
+	writer.Close()
+
+	resp, err := t.client.callAPI(
+		ctx,
+		requestParam{
+			path:   t.client.BaseURL + subURLTeamCreate,
+			method: http.MethodPost,
+			writer: writer,
+		},
+	)
+	if err != nil {
+		return Team{}, err
+	}
+
+	team := Team{}
+	err = json.NewDecoder(resp.Body).Decode(&team)
+	if err != nil {
+		return Team{}, err
+	}
+
+	return team, nil
+}
+
+func (t *TeamAPI) Update(ctx context.Context, teamName string) (Team, error) {
+	var payload bytes.Buffer
+	writer := multipart.NewWriter(&payload)
+
+	teamNameField, err := writer.CreateFormField(teamFieldName)
+	if err != nil {
+		return Team{}, err
+	}
+	teamNameField.Write([]byte(teamName))
+	writer.Close()
+
+	resp, err := t.client.callAPI(
+		ctx,
+		requestParam{
+			path:   t.client.BaseURL + subURLTeam,
+			method: http.MethodPost,
+			writer: writer,
+		},
+	)
 	if err != nil {
 		return Team{}, err
 	}
--- team_test.go
+++ team_test.go
@@ -58,3 +58,85 @@ func TestTeam_Get(t *testing.T) {
 		})
 	}
 }
+
+func TestTeam_Create(t *testing.T) {
+	is := is.New(t)
+
+	teamJSON := testdata.GetGolden(t, "team")
+
+	team := hellosign.Team{}
+	err := json.Unmarshal(teamJSON, &team)
+	is.NoErr(err)
+
+	tests := map[string]struct {
+		teamHTTPClient *http.Client
+		teamName       string
+		expectedTeam   hellosign.Team
+		expectedError  error
+	}{
+		"success": {
+			teamHTTPClient: testdata.MockHTTPClient(t, http.StatusOK, teamJSON, make(http.Header)),
+			teamName:       "Team HelloSign",
+			expectedTeam:   team,
+			expectedError:  nil,
+		},
+	}
+
+	for testName, test := range tests {
+		t.Run(testName, func(t *testing.T) {
+			is := is.New(t)
+
+			apiClient := hellosign.NewClient("123")
+			apiClient.HTTPClient = test.teamHTTPClient
+			resp, err := apiClient.TeamAPI.Create(context.TODO(), test.teamName)
+			if err != nil {
+				is.Equal(test.expectedError.Error(), err.Error())
+				return
+			}
+
+			is.NoErr(err)
+			is.Equal(test.expectedTeam, resp)
+		})
+	}
+}
+
+func TestTeam_Update(t *testing.T) {
+	is := is.New(t)
+
+	teamJSON := testdata.GetGolden(t, "team")
+
+	team := hellosign.Team{}
+	err := json.Unmarshal(teamJSON, &team)
+	is.NoErr(err)
+
+	tests := map[string]struct {
+		teamHTTPClient *http.Client
+		teamName       string
+		expectedTeam   hellosign.Team
+		expectedError  error
+	}{
+		"success": {
+			teamHTTPClient: testdata.MockHTTPClient(t, http.StatusOK, teamJSON, make(http.Header)),
+			teamName:       "Team HelloSign",
+			expectedTeam:   team,
+			expectedError:  nil,
+		},
+	}
+
+	for testName, test := range tests {
+		t.Run(testName, func(t *testing.T) {
+			is := is.New(t)
+
+			apiClient := hellosign.NewClient("123")
+			apiClient.HTTPClient = test.teamHTTPClient
+			resp, err := apiClient.TeamAPI.Create(context.TODO(), test.teamName)
+			if err != nil {
+				is.Equal(test.expectedError.Error(), err.Error())
+				return
+			}
+
+			is.NoErr(err)
+			is.Equal(test.expectedTeam, resp)
+		})
+	}
+}
